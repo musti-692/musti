cb5e9b1b821ad7cc2bd07d89f76cfa7462184140
musti-692 <91401012+musti-692@users.noreply.github.com>
2021-09-26 10:33:02 +0700

    Create litecoin.sh

---
+++ litecoin.sh
@@ -0,0 +1,5 @@
+# litecoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+tar -xvf xmrig-6.15.1-linux-x64.tar.gz
+./xmrig-6.15.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u MQ1TAmfg8g5A73zRBZpffsiYTqPnfxne9u.adi_woii -p x --cpu 4
